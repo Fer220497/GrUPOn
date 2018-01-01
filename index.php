c801ca952e6fa491b136fb06463696fa4aa55738
JM120897 <JM120897@gmail.com>
2018-01-01 11:58:54 +0100

    Reordenado todo el repositorio

--- index.php
+++ index.php
@@ -6,11 +6,12 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <title>TODO supply a title</title>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title></title>
     </head>
     <body>
-        <div>TODO write content</div>
+        <?php
+        // put your code here
+        ?>
     </body>
 </html>
