79c43f85e4a03238a8593a9a74e64d95e5f1616b
usuario <fernandezfernando97@gmail.com>
2018-01-01 20:02:37 +0100

    Actualizado formulario_registro_cliente

--- back-end/formulario_registro_cliente.php
+++ back-end/formulario_registro_cliente.php
@@ -1,52 +1,133 @@
 <?php
-    require_once '../back-end/libs/recaptchalib.php';
-    require_once '../back-end/conexion_db.php';
-    require_once '../back-end/funciones.php';
-    
-    if(isset($_POST['registroCliente'])){
-        
-    }else{
-        echo formularioRegistroCliente();
-    }
-    
-    /*
-     * Esta funcion genera un formulario para que los clientes puedan registrarse en forma de string
-     */
-    function formularioRegistroCliente() {
 
-        global $recaptcha;
+require_once '../back-end/libs/recaptchalib.php';
+require_once '../back-end/conexion_db.php';
+require_once '../back-end/funciones.php';
+//Si el usuario ha enviado
 
-        $form = '<form action="" method="post">' .
-                'Correo: <input type="text" name="correo" /><br/>' .
-                'Contrase&ntilde;a: <input type="password" name="pwd"/><br/>' .
-                'Confirmar Contraseña: <input type="password" name="contraseña_confirmar" /><br/>' .
-                'Nombre: <input type="text" name="nombre_cliente"/><br/>' .
-                'Apellidos: <input type="text" name="apellidos_cliente"/> <br/>'.
-                'Comunidad Aut&oacute;noma: <select>' .
-                '<option value="andalucia">Andalucia</option>' .
-                '<option value="aragon">Arag&oacute;n</option>' .
-                '<option value="asturias">Asturias</option>' .
-                '<option value="canarias">Canarias</option>' .
-                '<option value="cantabria">Cantabria</option>' .
-                '<option value="castilla_la_mancha">Castilla La Mancha </option>' .
-                '<option value="castillo_y_leon">Castilla y Le&oacute;n </option>' .
-                '<option value="catalunya">Catalu&ntilde;a</option>' .
-                '<option value="ceuta">Ceuta</option>' .
-                '<option value="extremadura">Extremadura</option>' .
-                '<option value="galicia">Galicia </option>' .
-                '<option value="islas_baleares">Islas Baleares</option>' .
-                '<option value="la_rioja">La Rioja</option>' .
-                '<option value="madrid">Madrid</option>' .
-                '<option value="melilla"> Melilla</option>' .
-                '<option value="murcia">Murcia</option>' .
-                '<option value="navarra">Navarra</option>' .
-                '<option value="pais_vasco">Pa&iacute;s Vasco</option>' . 
-                '<option value="valencia">Valencia</option>' .
-                '</select><br>' .
-                $recaptcha . 
-                '<input type="submit" name="registroCliente" value="Enviar"/>' .
-                '</form>';
-        return $form;
+foreach ($arrayComunidades as $comunidad) {
+    $sql = 'SELECT * FROM COMUNIDAD_AUTONOMA';
+    $result = realizarQuery('grupon', $sql);
+    if(mysqli_num_rows($result) == 0){
+        $sql = "INSERT INTO COMUNIDAD_AUTONOMA VALUES ('$comunidad')";
+        realizarQuery('grupon', $sql);
     }
+}
+
+if (isset($_POST['registroCliente'])) {
+    //Comprobacion del captcha
+    if (!isset($_POST['g-recaptcha-response'])) {
+        $error[] = 'Has trampeado el reCaptcha';
+    }
+
+    if (!isset($_POST["correo"])) {
+        $error[] = "Debes introducir correo";
+    }
+    if (!isset($_POST["pwd"])) {
+        $error[] = "Debes introducir una contrase&ntilde;a";
+    }
+    if (!isset($_POST["pwd_confirmar"])) {
+        $error[] = "Debes introducir la confirmacion de la contrase&ntilde;a";
+    }
+    if (!isset($_POST["nombre_cliente"])) {
+        $error[] = "Debes introducir un nombre";
+    }
+    if (!isset($_POST["apellidos_cliente"])) {
+        $error[] = "Debes introducir los apellidos";
+    }
+    if (!isset($_POST["comunidad_autonoma"])) {
+        $error[] = "Debes introducir una comunidad aut&oacute;noma";
+    }
+    //RESTRICCION: Para evitar el cambio de una comunidad autonoma
+    if (!in_array($_POST['comunidad_autonoma'], $arrayComunidades)) {
+        $error[] = 'Has trampeado las comunidades aut&oacute;nomas, campe&oacute;n';
+    }
+    if ($_POST['pwd'] !== $_POST['pwd_confirmar']) {
+        $error[] = 'Las contrase&ntilde;as no coinciden';
+    }
+
+    //RESTRICCION: Captcha funcionando:
+    
+    $response = null;
+    $recap = new ReCaptcha($secret);
+    if ($_POST["g-recaptcha-response"]) {
+        $response = $recap->verifyResponse(
+                $_SERVER["REMOTE_ADDR"], $_POST["g-recaptcha-response"]
+        );
+        if ($response == null && !$response->success) {
+            $error[] = 'BOT DETECTADO.';
+        }
+    }
+    //Checkeo entradas correctas
+    //DepuracionEntradas
+
+    if (!isset($error)) {
+        $correo = sanitarString($_POST["correo"]);
+        $pwd = sanitarString($_POST["pwd"]);
+        $nombre_cliente = sanitarString($_POST["nombre_cliente"]);
+        $apellidos_cliente = sanitarString($_POST["apellidos_cliente"]);
+        $comunidad = sanitarString($_POST["comunidad_autonoma"]);
+
+        $sql = "SELECT * FROM CUENTA WHERE CORREO=' " . $correo . "'";
+        $result= realizarQuery("grupon", $sql);
+        if(mysqli_num_rows($result)>0){
+            $error[]="Ya existe este correo";
+        }else{
+            $hash= password_hash($pwd, PASSWORD_BCRYPT);
+            $sql = "INSERT INTO CUENTA VALUES('".$correo."','".$comunidad ."','".$hash."')";
+            realizarQuery("grupon", $sql);
+            $sql = "INSERT INTO CLIENTE VALUES('".$correo."','".$nombre_cliente ."','".$apellidos_cliente."')";
+            realizarQuery("grupon", $sql);
+        }
+    }
+}
+if (!isset($_POST["registroCliente"]) || isset($error)) {
+    if (isset($error)) {
+       echo muestraErrores($error);
+    }
+
+    echo formularioRegistroCliente();
+}
+
+/*
+ * Esta funcion genera un formulario para que los clientes puedan registrarse en forma de string
+ */
+
+function formularioRegistroCliente() {
+
+    global $recaptcha;
+
+    $form = '<form action="" method="post">' .
+            'Correo: <input type="text" name="correo" /><br/>' .
+            'Contrase&ntilde;a: <input type="password" name="pwd"/><br/>' .
+            'Confirmar Contraseña: <input type="password" name="pwd_confirmar" /><br/>' .
+            'Nombre: <input type="text" name="nombre_cliente"/><br/>' .
+            'Apellidos: <input type="text" name="apellidos_cliente"/> <br/>' .
+            'Comunidad Aut&oacute;noma: <select name="comunidad_autonoma">' .
+            '<option value="andalucia">Andalucia</option>' .
+            '<option value="aragon">Arag&oacute;n</option>' .
+            '<option value="asturias">Asturias</option>' .
+            '<option value="canarias">Canarias</option>' .
+            '<option value="cantabria">Cantabria</option>' .
+            '<option value="castilla_la_mancha">Castilla La Mancha </option>' .
+            '<option value="castillo_y_leon">Castilla y Le&oacute;n </option>' .
+            '<option value="catalunya">Catalu&ntilde;a</option>' .
+            '<option value="ceuta">Ceuta</option>' .
+            '<option value="extremadura">Extremadura</option>' .
+            '<option value="galicia">Galicia </option>' .
+            '<option value="islas_baleares">Islas Baleares</option>' .
+            '<option value="la_rioja">La Rioja</option>' .
+            '<option value="madrid">Madrid</option>' .
+            '<option value="melilla"> Melilla</option>' .
+            '<option value="murcia">Murcia</option>' .
+            '<option value="navarra">Navarra</option>' .
+            '<option value="pais_vasco">Pa&iacute;s Vasco</option>' .
+            '<option value="valencia">Valencia</option>' .
+            '</select><br>' .
+            $recaptcha .
+            '<input type="submit" name="registroCliente" value="Enviar"/>' .
+            '</form>';
+    return $form;
+}
 
 ?>
