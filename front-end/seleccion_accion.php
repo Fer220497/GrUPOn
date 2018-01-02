af7c3308dee3e97d24d662083ee34045f0205bc1
usuario <fernandezfernando97@gmail.com>
2018-01-02 12:16:23 +0100

    Añadidas los archivos para creacion  de ctaalogos y productos

---
+++ front-end/seleccion_accion.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h2>Login</h2>
+       
+        Seleccione que quiere hacer<br/>
+        <button type="submit" onclick="location.href = 'creacion_catalogo.php'">Crear Cat&aacute;logo</button> 
+        <button type="submit" onclick="location.href = 'creacion_producto.php'">Crear Producto</button> 
+        
+    </body>
+</html>
