6a7a11476e769d778c0d04610787cace8aabde23
JM120897 <JM120897@gmail.com>
2018-01-03 13:34:37 +0100

    Creada la función 'existeCorreo'

--- back-end/formulario_registro_empresa.php
+++ back-end/formulario_registro_empresa.php
@@ -141,27 +141,7 @@ function formularioRegistroEmpresa() {
             'Cuenta Bancaria: <input type="number" name="cuenta_bancaria"/ ><br/>' .
             'Tel&eacute;fono: <input type="number" name="telefono_empresa"/><br/>' .
             'Correo Electr&oacute;nico: <input type="email" name="mail_empresa"/> <br/>' .
-            'Comunidad Aut&oacute;noma: <select name="comunidad_autonoma">' .
-            '<option value="andalucia">Andalucia</option>' .
-            '<option value="aragon">Arag&oacute;n</option>' .
-            '<option value="asturias">Asturias</option>' .
-            '<option value="canarias">Canarias</option>' .
-            '<option value="cantabria">Cantabria</option>' .
-            '<option value="castilla_la_mancha">Castilla La Mancha </option>' .
-            '<option value="castillo_y_leon">Castilla y Le&oacute;n </option>' .
-            '<option value="catalunya">Catalu&ntilde;a</option>' .
-            '<option value="ceuta">Ceuta</option>' .
-            '<option value="extremadura">Extremadura</option>' .
-            '<option value="galicia">Galicia </option>' .
-            '<option value="islas_baleares">Islas Baleares</option>' .
-            '<option value="la_rioja">La Rioja</option>' .
-            '<option value="madrid">Madrid</option>' .
-            '<option value="melilla"> Melilla</option>' .
-            '<option value="murcia">Murcia</option>' .
-            '<option value="navarra">Navarra</option>' .
-            '<option value="pais_vasco">Pa&iacute;s Vasco</option>' .
-            '<option value="valencia">Valencia</option>' .
-            '</select><br>' .
+            'Comunidad Aut&oacute;noma: '.$selectComunidadesAutonomas.'<br>' .
             'Direcci&oacute;n Empresa: <input type="text" name="direccion_empresa" />'.
             $recaptcha .
             '<input type="submit" name="registroEmpresa" value="Enviar"/>' .
