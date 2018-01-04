cfb0aeee47e84a1bae23f5ff5736f235b90ed68f
pedro1512 <zamoranpedro12@gmail.com>
2018-01-04 16:10:11 +0100

    modificar producto

--- back-end/formulario_modificar_producto.php
+++ back-end/formulario_modificar_producto.php
@@ -20,12 +20,20 @@ if (isset($_POST['modificarProducto'])) {
         $errores[] = 'Debes introducir porcentaje de descuento.';
     }
     if (!isset($errores)) {
+        $sql = 'SELECT * FROM PRODUCTO WHERE id_producto = "' . 2 . '"';
+        $result = realizarQuery('grupon', $sql);
+        $producto = mysqli_fetch_row($result);
+        $cantidadVendida = $producto[9];
         $nombre = $_POST['nombre'];
         $precio = $_POST['precio'];
         $descripcion = $_POST['descripcion'];
         $localizacion = $_POST['localizacion'];
         $porcentaje_descuento = $_POST['porcentaje_descuento'];
-        $sql = "UPDATE PRODUCTO SET nombre = '" . $nombre . "', precio = '" . $precio . "', descripcion = '" . $descripcion . "', localizacion = '" . $localizacion . "', porcentaje_descuento = '" . $porcentaje_descuento . "' WHERE id_producto = 2";
+        $categoria = $_POST['categoria'];
+        $comunidad = $_POST['comunidad'];
+        $cantidad = $_POST['cantidad'];
+        $total = $cantidadVendida + $cantidad;
+        $sql = "UPDATE PRODUCTO SET nombre_categoria = '" . $categoria . "', nombre_ca = '" . $comunidad . "', nombre = '" . $nombre . "', precio = '" . $precio . "', descripcion = '" . $descripcion . "', localizacion = '" . $localizacion . "', cantidad_disponible = '" . $cantidad . "', cantidad_total ='" . $total . "', porcentaje_descuento = '" . $porcentaje_descuento . "' WHERE id_producto = 2";
         realizarQuery('grupon', $sql);
     }
 }
@@ -37,17 +45,18 @@ if (!isset($_POST['modificarProducto']) || isset($errores)) {
 }
 
 function formularioModificarProducto($id_producto) {
-
     $sql = 'SELECT * FROM PRODUCTO WHERE id_producto = "' . $id_producto . '"';
     $result = realizarQuery('grupon', $sql);
     $producto = mysqli_fetch_row($result);
-
     $form = '<form action="" method="post">' .
             'Nombre: <input type="text" name="nombre" value="' . $producto[4] . '"/><br/>' .
             'Precio: <input type="number" name="precio" value="' . $producto[5] . '"/><br/>' .
-            'Descripci&oacute;n: <textarea name="descripcion" rows="4" cols="10">' .$producto[6] . '</textarea><br/>' .
+            'Descripci&oacute;n: <textarea name="descripcion" rows="4" cols="10">' . $producto[6] . '</textarea><br/>' .
+            'Categor&iacute;a: <select name="categoria">' . optionCategoriasSeleccionadas($producto[1]) . '</select><br/>' .
+            'Comunidad aut&oacute;noma: <select name="comunidad">' . opcionesComunidadSeleccionada($producto[2]) . '</select><br/>' .
             'Localizaci&oacute;n: <input type="text" name="localizacion" value="' . $producto[7] . '" /><br/>' .
-            'Porcentaje Descuento: <input type="number" name="porcentaje_descuento" value="' . $producto[8] . '" /><br/>' .
+            'Porcentaje descuento: <input type="number" name="porcentaje_descuento" value="' . $producto[8] . '" /><br/>' .
+            'Cantidad disponible: <input type="number" name="cantidad" value="' . $producto[11] . '" /><br/>' .
             '<input type="submit" name="modificarProducto" value="Enviar"/>' .
             '</form>';
     return $form;
