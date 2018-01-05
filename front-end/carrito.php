4c5f591111ced87a845a7cac018f409aa6474a91
JM120897 <JM120897@gmail.com>
2018-01-05 12:43:26 +0100

    Carrito funcionando

--- front-end/carrito.php
+++ front-end/carrito.php
@@ -3,12 +3,15 @@
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <script>
     </head>
     <body>
         <?php
         require_once '../back-end/lectura_producto.php';
         require_once '../back-end/formulario_carrito.php';
-        $carrito = $_COOKIE['carrito'];
+        $carrito = explode(',',$_COOKIE['carrito']);
+        echo $_COOKIE['carrito'];
+        echo print_r($carrito);
         foreach($carrito as $id_prod){
             echo muestraProducto($id_prod);
             echo borrarProductoCarrito($id_prod);
