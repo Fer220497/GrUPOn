3836015f499d72a7e90920fa3c19dd513ee5ff0e
JM120897 <JM120897@gmail.com>
2018-01-06 17:36:32 +0100

    Muestra empresa funcionando al 100%

--- front-end/index.php
+++ front-end/index.php
@@ -1,20 +1,22 @@
+<?php
+session_start();
+require_once '..\back-end\funciones.php';
+?>
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
-        <?php
-        require_once '..\back-end\funciones.php';
-        ?>
-        <meta charset="UTF-8">
+
         <title>GrUPOn</title>
-
-        <script src="..\libs\cookieInterface.js">
-
-        </script>
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+        <link rel="icon" href="img/logo.png">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            body{
+                font-family: 'Roboto', sans-serif;
+            }
+        </style>
+        <script src="..\libs\cookieInterface.js"></script>
         <script>
             var categorias = {viajes: "Viajes",
                 entretenimiento: "Entretenimiento",
@@ -29,37 +31,36 @@ and open the template in the editor.
     </head>
     <body>
         <header>
-            <div><img alt="GrUPOn" src="..\img\logo.png" height="100"/></div>
-            <h1 id="tit_cabecera">GrUPOn</h1>
-            <?php
-            require_once 'buscar_producto.php';
-            if (!isset($_SESSION["cuenta"])) {
-                ?>
-                <div><a href="login.php">Login</a></div>
-                <?php
-            } else {
-                ?>
-                <div><a href="cuenta.php">Perfil</a></div>
-                <?php
-            }
-            ?>
+            <header>
+                <div id="logo">
+                    <a href="index.php"><img alt="GrUPOn" src="..\img\logo.png" height="100"/></a>
+                </div>
+                <div id="banner">
+                    <a href="index.php">
+                    </a>
+                    <h1>GrUPOn</h1>
+                </div>
+            </header>
+            <nav>
 
-        </header>
-        <nav id="side-nav">
-            <?php
-            echo menuCategorias();
-            ?>
-        </nav>
-        <section id="body">
-            <h2 id="categoria_actual"><div id="cookie"></div><script>$("#cookie").append(document.createTextNode(categorias[getCookie('categoria')]));</script></h2>
-            <p>
-                <?php
-                desplegarPaginaPrincipal();
-                ?>
-            </p>
-        </section>
-        <footer>
-            Grupo &num;2 - GrUPOn&copy;, el fruto dado por el odio hacia nosotros mismos
-        </footer>
+            </nav>
+            <main>
+                <aside>
+                    <?php
+                    echo menuCategorias();
+                    ?>
+                </aside>
+
+                <article>
+                    <h2 id="categoria_actual"><div id="cookie"></div><script>$("#cookie").append(document.createTextNode(categorias[getCookie('categoria')]));</script></h2>
+                            <?php
+                            desplegarPaginaPrincipal();
+                            ?>
+                </article>
+            </main>
+
+            <footer>
+                Grupo &num;2 - GrUPOn&copy;, el fruto dado por el odio hacia nosotros mismos
+            </footer>
     </body>
 </html>
