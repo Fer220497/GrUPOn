6096a81e486a3134fda5146344a484ae7182f497
usuario <fernandezfernando97@gmail.com>
2018-01-06 18:38:32 +0100

    Añadido Nav horizontal

--- back-end/funciones.php
+++ back-end/funciones.php
@@ -295,6 +295,27 @@ function desplegarPaginaPrincipal() {
     }
 }
 
+function navigation() {
+    $nav = '<div id="horizontalNav"><ul>';
+    if (isset($_SESSION["cuenta"])) {
+        if ($_SESSION["tipo"] == "cliente") {// LOGEADO COMO CLIENTE
+            $nav = '<li><a id="carrito" href="mostrar_carrito.php">Carrito</a></li>';
+                    '<li><a id="perfil" href="">Perfil</a></li>' .
+                    '<li><a id="logout" href="">Desconectar</a></li>' ;
+                    
+        } else {// LOGEADO COMO EMPRESA
+            $nav .= '<li><a id="crear_produto" href="creacion_producto.php">Crear Producto</a></li>' .
+                    '<li><a id="crear_catalogo" href="creacion_catalogo.php">Crear Catalogo</a></li>' .
+                    '<li><a id="perfil" href="">Perfil</a></li>' .
+                    '<li><a id="logout" href="">Desconectar</a></li>' ;
+        }
+    } else {//NO LOGEADO
+        $nav .= '<li><a id="boton_login" href="login.php">Login|Registro</a></li>' .
+                '<li><a id="carrito" href="mostrar_carrito.php">Carrito</a></li>';
+    }
+    $nav .= " </ul></div>";
+}
+
 /* * TAMAÑOS MAXIMOS DE LAS VARIABLES EN LA DB* */
 $tamCorreo = 75;
 $tamNombreEmpresa = 100;
