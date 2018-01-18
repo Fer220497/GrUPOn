386293893a4f346d8e87f94a3628d737af63675a
JM120897 <JM120897@gmail.com>
2018-01-18 23:40:52 +0100

    Intento w3css

--- back-end/funciones.php
+++ back-end/funciones.php
@@ -448,13 +448,16 @@ function busquedaCatalogo() {
 }
 
 function formularioBusquedaProducto() {
-    $form = '<form action="busqueda.php" method="get"><div id="busqueda">' .
-            '<input id="cuadro_busqueda" type="text" name="nombre" placeholder="Busca algo!"/>';
-    $form .= '<input id="boton_busqueda" type="submit" value="Buscar" name="busqueda"/>';
     if (isset($_SESSION["cuenta"])) {
-        $form .= '<div id="checknacional"> Nacional <input type="checkbox" name="nacional" value="nacional"/></div>';
+        $form = '<form class="w3-row" action="busqueda.php" method="get">' .
+                '<input class="w3-input w3-col m8 w3-bar-item w3-input w3-white w3-hide-small" type="text" name="nombre" placeholder="Busca algo!"/>' .
+                '<input class="w3-col m2 w3-btn w3-flat-clouds w3-center" type="submit" value="🔍" name="busqueda"/>' .
+                '<div class="w3-col m2">Nacional <input type="checkbox" name="nacional" value="nacional"/></form>';
+    } else {
+        $form = '<form class="w3-row" action="busqueda.php" method="get">' .
+                '<input class="w3-col m8 w3-input w3-bar-item w3-input w3-white w3-hide-small" type="text" name="nombre" placeholder="Busca algo!"/>' .
+                '<input class="w3-col m4 w3-btn w3-flat-clouds w3-cell" type="submit" value="🔍" name="busqueda"/></form>';   
     }
-    $form .= '</div></form>';
 
     return $form;
 }
